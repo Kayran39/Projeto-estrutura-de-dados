644d89d8e9fc5e56a5930419371aa954f0aad0a0
Kayran <kayran.silva@mail.uft.edu.br>
2025-05-17 09:18:57 -0300

    Projeto estrutura de dados V0.5

---
+++ ProjetoEstruttradedados/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define POP_SIZE 100
+#define MAX_MOVES 50
+#define MUTATION_RATE 0.05
+#define MAX_GENERATIONS 500
+#define TOURNAMENT_SIZE 3
+#define ROWS 7
+#define COLS 10
+
+#endif
